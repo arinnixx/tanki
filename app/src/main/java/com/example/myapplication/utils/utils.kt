d115b1d74076aed08b07a6f779222d0a91f32b4e
arinnixx <tyrtyshnaya.a.0@mail.ru>
2025-02-01 22:42:44 +0300

    presentation 11

--- app/src/main/java/com/example/myapplication/utils/utils.kt
+++ app/src/main/java/com/example/myapplication/utils/utils.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication.utils
 
 import android.view.View
+import com.example.myapplication.CELL_SIZE
 import com.example.myapplication.binding
 import com.example.myapplication.models.Coordinate
 import com.example.myapplication.models.Element
@@ -12,7 +13,22 @@ fun View.checkViewCanMoveThroughBorder(coordinate: Coordinate):Boolean{
             coordinate.left + this.width <= binding.container.width
 }
 
-fun getElementByCoordinates(coordinate: Coordinate, elementsOnContainer:List<Element>):Element?{
-
+fun getElementByCoordinates(
+    coordinate: Coordinate,
+    elementsOnContainer:List<Element>
+):Element?{
+    for (element in elementsOnContainer){
+        for(height in 0 until  element.height){
+            for (width in 0 until element.width){
+                val searchingCoordinate = Coordinate(
+                    top=element.coordinate.top+height * CELL_SIZE,
+                    left=element.coordinate.left+width * CELL_SIZE
+                )
+                if (coordinate==searchingCoordinate){
+                    return element
+                }
+            }
+        }
+    }
+return null
 }
-    elementsOnContainer.firstOrNull { it.coordinate==coordinate }
